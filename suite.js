f2f3ef45e66f9b09b486d705727761ef2b3bd317
dandeto <danielmichaelmills@gmail.com>
2017-01-28 12:54:06 -0600

    Directions, Fonts, and Small Fixes
    Added wiping function to stop displaying svg, as well as displaying
    directions. Added full directions. Random fixes.

--- suite.js
+++ suite.js
@@ -1,20 +1,48 @@
 //global vars
+var svg = document.getElementById("svg");
 var direct = document.querySelectorAll(".directions");
 var display = true;
 
 function wipe() {
-	alert("wipe");
+	console.log("wiping svg");
+  	svg.style.transition = "opacity 0.5s linear 0s";
+	svg.style.opacity = 0;
+	setTimeout(function(){
+  		svg.style.display = "none";
+	}, 500);
+}
+
+function restore() {
+	console.log("restoring svg");
+	svg.style.transition = "opacity 0.5s linear 0s";//no transition...
+	svg.style.opacity = 1;
+	setTimeout(function(){
+  		svg.style.display = "inline";
+	}, 500);
 }
 
 function directions() {
 	switch(display) {
 		case true:
+			wipe();
 			console.log("directions displayed");
-			direct[0].style.display = "inline";
-			display = false;
+			setTimeout(function(){
+				direct[0].style.transition = "opacity 0.5s linear 0s";//no transition...
+				direct[0].style.opacity = 1;
+				setTimeout(function(){
+					direct[0].style.display = "inline";
+					display = false;
+				}, 1050);
+			}, 550);
 			break;
 		case false:
-			console.log("directions displayed");
+			restore();
+			console.log("directions deleted");
+			direct[0].style.transition = "opacity 0.5s linear 0s";
+			direct[0].style.opacity = 0;
+			setTimeout(function(){
+  		direct[0].style.display = "none";
+	}, 500);
 			direct[0].style.display = "none";
 			display = true;
 			break;
